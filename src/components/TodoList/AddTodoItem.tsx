92c3cc400f136959a37d255fa596d3932ae4d389
prasoonk587 <prasoonk587@gmail.com>
2026-06-12 14:51:08 +0530

    Configured linting and prettier

--- src/components/TodoList/AddTodoItem.tsx
+++ src/components/TodoList/AddTodoItem.tsx
@@ -1,24 +1,26 @@
-import { FC, useState } from "react"
-import { ITodoItem } from "."
+import { FC, useState } from 'react';
+import { ITodoItem } from '.';
 
 interface IAddTodoItemProps {
-    addItem: (item: ITodoItem) => void
+    addItem: (item: ITodoItem) => void;
 }
 
-export const AddTodoItem: FC<IAddTodoItemProps> = ({addItem}) => {
-    const [description, setDescription] = useState<string | null>(null)
+export const AddTodoItem: FC<IAddTodoItemProps> = ({ addItem }) => {
+    const [description, setDescription] = useState<string | null>(null);
 
-    const onChange = (e: any) =>{
-        setDescription(e.target.value)
-    }
+    const onChange = (e: any) => {
+        setDescription(e.target.value);
+    };
 
     const onAddItem = () => {
-        addItem({description: description, isCompleted: false})
-        setDescription(null)
-    }
+        addItem({ description: description, isCompleted: false });
+        setDescription(null);
+    };
 
-    return <div style={{display: 'flex', padding: '20px 0'}}>
-            <input placeholder="Task Description" value={description ||''} onChange={onChange} />
+    return (
+        <div style={{ display: 'flex', padding: '20px 0' }}>
+            <input placeholder="Task Description" value={description || ''} onChange={onChange} />
             <button onClick={onAddItem}>Add Item</button>
         </div>
-}
+    );
+};
